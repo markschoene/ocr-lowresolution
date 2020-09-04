Decoder improvements:  * saves past to reduce re-computation  * fixed bug with building the TF Graph yielding to massive speedup  * deals with blank space at the end of words

softmax_tools/decoder.py
@@ -1,5 +1,7 @@
 # Python Library
-import re
+from copy import deepcopy
+import time
+import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.backend import ctc_decode
@@ -115,6 +117,30 @@ class LanguageDecoder(Decoder):
         self.beam_width = beam_width
         self.model = language_models.GPTModel(model_name=model_name, model_dir=model_dir, session=session)
         self.enc = self.model.encoder
+        self.history = '<|endoftext|>'
+
+        # load model parameters
+        saver = tf.train.Saver()
+        ckpt = tf.train.latest_checkpoint(os.path.join(self.model.model_dir,
+                                                       self.model.model_name))
+        saver.restore(session, ckpt)
+
+    @staticmethod
+    def remove_blank_at_beam_end(beams, probs, blank_char):
+        new_beams, new_probs = [], []
+        for j in range(len(beams)):
+            b = deepcopy(beams[j])
+            while b[-1] == blank_char and len(b) > 1:
+                b = b[:-1]
+            if b == beams[j]:
+                new_beams.append(b)
+                new_probs.append(probs[j])
+            else:
+                if b not in beams:
+                    new_beams.append(b)
+                    new_probs.append(probs[j])
+
+        return new_beams, new_probs
 
     def decode_line(self, df):
         """
@@ -124,7 +150,8 @@ class LanguageDecoder(Decoder):
         """
 
         # get blanks and remove duplicates
-        blanks = np.arange(len(df))[0 == np.argmax(df.values, axis=1)].tolist()
+        blank_char = df.columns.get_loc(" ")
+        blanks = np.arange(len(df))[blank_char == np.argmax(df.values, axis=1)].tolist()
         separator = [0, blanks[0]]
         separator.extend([blanks[i] for i in range(1, len(blanks)) if blanks[i] != blanks[i - 1] + 1])
         separator.append(len(df) - 1)
@@ -132,34 +159,51 @@ class LanguageDecoder(Decoder):
         # loop the words and merge language model outputs with beam search outputs
         output = ""
         for i in range(1, len(separator)):
+
+            # do beam search
             seq = df.iloc[separator[i - 1] + 1:separator[i]]
+
+            start_beam_search = time.time()
+            # TODO: Include removal of blanks in beam search
             beams, probs = ctcBeamSearch(mat=seq.values,
                                          blankIdx=len(df.columns) - 1,
                                          beamWidth=self.beam_width)
-            beams = [re.sub(r"\s+$", "",
-                            self._decode_sequence(np.array(beam), df.columns),
-                            flags=re.UNICODE)
-                     for beam in beams]
+            end_beam_search = time.time()
+
+            # prepare beams for NLP
+            beams, probs = self.remove_blank_at_beam_end(beams=beams, probs=probs, blank_char=blank_char)
+            beams = [self._decode_sequence(list(beam), df.columns) for beam in beams]
             beams_encoded = [self.enc.encode(beam) for beam in beams]
 
             # get past transformer attention to reduce compute
             context = self.enc.encode(self.history)
-            past = self.get_past(context)
+            if not hasattr(self, 'past'):
+                self.past = self.get_past(context)
 
             # compute NLP scores for beams
-            scores = np.zeros(self.beam_width)
+            scores = np.zeros(len(beams))
+            past_list = []
             for j, tokens in enumerate(beams_encoded):
-                s = self.model.score(tokens=tokens, past=past)
-                scores[j] = s
-                print(self.enc.decode(context[-20:]), f"<{self.enc.decode(tokens)} | {np.exp(s)} | {tokens}>")
+                score, past = self.model.score(tokens=tokens, past=self.past)
+                scores[j] = score
+                past_list.append(past)
+                print(self.history[-20:], f"<{self.enc.decode(tokens)} | {np.exp(score)} | {tokens}>")
 
             # merge NLP scores with OCR scores
-            merged_scores = (scores + np.log(probs) / len(df)) / 2
-            best_beam = beams[np.argmax(merged_scores)]
-            output += best_beam
-            context.extend(self.enc.encode(best_beam))
+            end_nlp = time.time()
+            print(f"Time for a) beam search: {end_beam_search - start_beam_search} b) nlp: {end_nlp - end_beam_search}")
+            merged_scores = (scores + np.log(probs)) / 2
+            best_beam = np.argmax(merged_scores)
+            output += beams[best_beam]
+            self.history += beams[best_beam]
+            self.past = past_list[best_beam]
+            del past_list
 
         return output
+
+    def clear_past(self):
+        if self.past:
+            self.past = None
 
     def get_past(self, context):
         context_tokens = [context] if context else [self.enc.encoder['<|endoftext|>']]

softmax_tools/language_models.py
@@ -41,31 +41,23 @@ class GPTModel(LanguageModel):
         # Byte Pair Encoder
         self.encoder = gpt.encoder.get_encoder(model_name, model_dir)
 
-        # parameters for inference
-        self.num_classes = len(self.encoder.bpe_ranks.keys())
+        # build graph
+        self.c = tf.placeholder(tf.int32, [1, None])
+        self.past = None
+        self.output = self.inference(tokens=self.c)
 
-    def init_saver(self):
-        self.saver = tf.train.Saver()
-        self.ckpt = tf.train.latest_checkpoint(os.path.join(self.model_dir, self.model_name))
-        self.saver.restore(self.session, self.ckpt)
+    def inference(self, tokens):
+        lm_output = gpt.model.model(hparams=self.hparams, X=tokens, past=self.past, reuse=tf.AUTO_REUSE)
 
-    def inference(self, tokens, past=None):
-        lm_output = gpt.model.model(hparams=self.hparams, X=tokens, past=past, reuse=tf.AUTO_REUSE)
-
-        present = lm_output['present'] if past is None else tf.concat([past, lm_output['present']], axis=-2)
+        present = lm_output['present'] if self.past is None else tf.concat([self.past, lm_output['present']], axis=-2)
         logits = lm_output['logits'][:, :, :self.hparams.n_vocab]
 
         return logits, present
 
     def step(self, context_tokens, past=None):
-        c = tf.placeholder(tf.int32, [1, None])
-        output = self.inference(tokens=c, past=past)
-        if not hasattr(self, 'saver'):
-            self.init_saver()
-
-        logits, present = self.session.run(output, feed_dict={
-            c: context_tokens
-
+        self.past = past
+        logits, present = self.session.run(self.output, feed_dict={
+            self.c: context_tokens
         })
 
         return logits[0], present
@@ -78,4 +70,4 @@ class GPTModel(LanguageModel):
                         for i in range(len(tokens))]
         score = np.sum(token_scores) / len(tokens)
 
-        return score
+        return score, present